22e885be88582557f05f454477200f793b823a15
Abdulrasheed Ibrahim <abdulrasheedibrahim47@gmail.com>
2021-11-03 14:41:33 +0100

    Commented onesignal

--- farpush/views.py
+++ farpush/views.py
@@ -2,32 +2,32 @@ from .models import Message
 from .forms import MessageForm
 from django.shortcuts import render
 from django.contrib import messages
+from django.http import JsonResponse
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
 
 import onesignal as onesignal_sdk
-from onesignalkeys import APP_AUTH_KEY, APP_ID
+#from onesignalkeys import APP_AUTH_KEY, APP_ID
 
-def send_message(title, message):
+#def send_message(title, message):
 	# create a onesignal client
-	onesignal_client = onesignal_sdk.Client(app_auth_key=APP_AUTH_KEY, app_id=APP_ID)
+	#onesignal_client = onesignal_sdk.Client(app_auth_key=APP_AUTH_KEY, app_id=APP_ID)
 
 	# create a notification
-	new_notification = onesignal_sdk.Notification(post_body={
-		"headings": {"en": title},
-	    "contents": {"en": message},
-	    "included_segments": ["Subscribed Users"]
-	})
+	# new_notification = onesignal_sdk.Notification(post_body={
+	# 	"headings": {"en": title},
+	#     "contents": {"en": message},
+	#     "included_segments": ["Subscribed Users"]
+	# })
 
 	# send notification, it will return a response
-	try:
-		onesignal_response = onesignal_client.send_notification(new_notification)
-		print(onesignal_response.status_code)
-		print(onesignal_response.json())
-	except:
-		print("err")
-		return False
+	# try:
+	# 	onesignal_response = onesignal_client.send_notification(new_notification)
+	# 	print(onesignal_response.status_code)
+	# 	print(onesignal_response.json())
+	# except:
+	# 	print("err")
+	# 	return False
 
 @login_required
 def home(request):
@@ -47,7 +47,7 @@ def home(request):
 			return redirect('home')
 		else:
 			form = MessageForm(request.POST)
-			context = {'form': form}
+			context = { 'form': form }
 			return render(request, temp, context)
 	all_messages = Message.objects.all().order_by('-id')
 	return render(request, temp, {'all_messages': all_messages})
