0717ed66c65cd3973dccb22151946e1f149d5e09
Abdulrasheed Ibrahim <abdulrasheedibrahim47@gmail.com>
2021-11-03 15:38:50 +0100

    Update

--- farpush/models.py
+++ farpush/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
-# Create your models here.
-
 class Message(models.Model):
-	title = models.CharField(max_length=30)
+	title = models.CharField(max_length=100)
 	body = models.TextField()
 	timestamp = models.DateTimeField(auto_now_add=True)
 
