9d2fe6c8e16941bf30c233255b2c63e6c0ddd1f1
Abdulrasheed Ibrahim <abdulrasheedibrahim47@gmail.com>
2021-11-10 06:31:22 +0100

    Add Message to admin

--- farpush/admin.py
+++ farpush/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Message
 
+
+admin.site.register(Message)
+
 admin.site.site_header = "Information Tech. Push Notification Service"
 admin.site.site_title = "Information Tech. Push Notification System"
 
